576bfac96087e3243f6c2cc305748726ff74361a
i am moein <44437189+moeinsk@users.noreply.github.com>
2019-02-22 06:47:04 -0800

    Add files via upload

---
+++ Triangle.py
@@ -0,0 +1,14 @@
+#writed by moein
+print("Enter Number:")
+N = int(input())
+
+numbers = []
+
+for i in range(N):
+    numbers.append("*")
+    print(''.join(numbers))
+    if i == N-1:
+        for m in range(N):
+            numbers.pop()
+            print(''.join(numbers))
+#thise is python's power           
